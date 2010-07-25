e52b0eb117551663bf5dbeec0888ab33ab542bd9
Benson Lim <pugnusferreus@192-168-1-6.tpgi.com.au>
2010-07-25 20:16:29 +1000

    more test

--- src/com/progriff/jhaml/test/BatchJHamlTest.java
+++ src/com/progriff/jhaml/test/BatchJHamlTest.java
@@ -1,5 +1,9 @@
 package com.progriff.jhaml.test;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,11 +18,13 @@ public class BatchJHamlTest extends TestCase {
     private final static String SEPERATOR = 
                                     System.getProperty("file.separator");
     
+    private String hamlPath = CURRENT_PATH + SEPERATOR + "unitTest" + SEPERATOR + "hamlTest"; 
+    private String hamlLayoutPath = CURRENT_PATH + SEPERATOR + "unitTest" + SEPERATOR + "hamlTest" + SEPERATOR + "layouts"; 
+    private String outputPath = CURRENT_PATH + SEPERATOR + "unitTest" + SEPERATOR + "outputTest";
+    
     @Before
     public void setUp() {
-        String hamlPath = CURRENT_PATH + SEPERATOR + "unitTest" + SEPERATOR + "hamlTest";
-        String outputPath = CURRENT_PATH + SEPERATOR + "unitTest" + SEPERATOR + "outputTest";
-        batchJHaml = new BatchJHaml(hamlPath,outputPath,"jsp");
+        batchJHaml = new BatchJHaml(hamlPath,hamlLayoutPath,outputPath,"jsp");
     }
     
     @Test
@@ -31,5 +37,35 @@ public class BatchJHamlTest extends TestCase {
     public void testGetInvalidLayout() {
         batchJHaml.populateLayouts();
         assertNull(batchJHaml.getLayouts().get("test.haml"));
+    }
+    
+    @Test
+    public void testGetFolderWithNoLayout() {
+        batchJHaml.setHamlLayoutPath(outputPath);
+        batchJHaml.populateLayouts();
+        assertTrue(batchJHaml.getLayouts().size() < 1);
+        batchJHaml.setHamlPath(hamlLayoutPath);
+    }
+    
+    @Test
+    public void testGetInvalidLayoutFolder() {
+        try {
+            batchJHaml.setHamlLayoutPath("sdfsdf");
+            batchJHaml.populateLayouts();
+            assertTrue(false);
+        }
+        catch(Exception e) {
+            assertTrue(true);
+        } finally {
+            batchJHaml.setHamlLayoutPath(hamlLayoutPath);
+        }
+        
+    }
+    
+    @Test
+    public void testGetContentOutput() throws IOException {
+        File hamlFile = new File(hamlPath + SEPERATOR + "test.haml");
+        assertFalse(StringUtils.isBlank(batchJHaml.getHamlOutput(hamlFile)));
+        assertFalse(StringUtils.contains(batchJHaml.getHamlOutput(hamlFile),"<html>"));
     }
 }
