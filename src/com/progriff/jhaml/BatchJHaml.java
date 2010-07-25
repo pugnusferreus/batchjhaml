cb9bae278b13398f0a611682d91cfcfa6a4dd3e5
Benson Lim <pugnusferreus@192-168-1-6.tpgi.com.au>
2010-07-25 18:18:29 +1000

    added javadocs

--- src/com/progriff/jhaml/BatchJHaml.java
+++ src/com/progriff/jhaml/BatchJHaml.java
@@ -10,24 +10,54 @@ import org.apache.commons.lang.StringUtils;
 
 import com.cadrlife.jhaml.JHaml;
 
+/**
+ * 
+ * @author Benson Lim
+ * Creates JSP or HTML in batch ala Staticmatic style.
+ */
 public class BatchJHaml {
-
+    /**
+     * Path where the haml files are
+     */
     private String hamlPath;
+    /**
+     * Path where the output files will be generated
+     */
     private String outputPath;
+    /**
+     * The output's extension.
+     */
     private String outputExtension;
-    
+    /**
+     * The current path where the application will run
+     */
     private final static String CURRENT_PATH = System.getProperty("user.dir");
+    /**
+     * File seperator.
+     */
     private final static String SEPERATOR = 
                                     System.getProperty("file.separator");
-    
+    /**
+     * Haml can have multiple layouts. This map is to store them. BatchJHaml
+     * only supports application.haml at the moment
+     */
     private HashMap<String, String> layouts = new HashMap<String, String>();
     
+    /**
+     * Default constructor.
+     */
     public BatchJHaml() {
         this(CURRENT_PATH + SEPERATOR + "haml", 
                 CURRENT_PATH + SEPERATOR + "output", 
                 "jsp");
     }
     
+    /**
+     * Hate default values? Call this one
+     * @param hamlPath Path to your Haml folder
+     * @param outputPath Path to output
+     * @param outputExtension The name of the extension to append
+     */
     public BatchJHaml(String hamlPath,
                         String outputPath,
                         String outputExtension) {
@@ -36,6 +66,10 @@ public class BatchJHaml {
         this.outputExtension = outputExtension;
     }
     
+    /**
+     * If you are not generating via ant run , you can call this function
+     * from your app.
+     */
     public void generateOutput() {
         File file = new File(this.hamlPath);
         populateLayouts();
@@ -43,7 +77,12 @@ public class BatchJHaml {
         for(File hamlFile: file.listFiles(getFilenameFilter())) {
             try {
                 String hamlOutput = getHamlOutput(hamlFile);
-                File outputFile = new File(this.outputPath + SEPERATOR + StringUtils.replace(hamlFile.getName(), ".haml", "." + this.outputExtension));
+                File outputFile = new File(
+                        this.outputPath + 
+                        SEPERATOR + 
+                        StringUtils.replace(hamlFile.getName(), ".haml", "." + 
+                                this.outputExtension));
+                
                 writeToFile(outputFile, hamlOutput);
             }
             catch(IOException ioe) {
@@ -52,6 +91,14 @@ public class BatchJHaml {
         }
     }
     
+    /**
+     * Returns a filter that accepts filenames that: <br/>
+     * <ul>
+     *   <li/> doesn't start with "."
+     *   <li/> ends with ".haml"
+     * </ul>
+     * @return the FilenameFilter object.
+     */
     public FilenameFilter getFilenameFilter() {
         return new FilenameFilter() {
             public boolean accept(File dir, String name) {
@@ -60,27 +107,49 @@ public class BatchJHaml {
         };
     }
     
-    
-    private void writeToFile(File outputFile, String hamlOutput) throws IOException {
+    /**
+     * Writes the output to the output folder. Replaces <%= yield %> to use
+     * the content markup
+     * @param outputFile File object to output
+     * @param hamlOutput The hamlOutput from JHaml
+     * @throws IOException if unable to write to the folder for some reason
+     */
+    private void writeToFile(File outputFile, String hamlOutput) 
+                                                    throws IOException {
+        
         String layout = this.layouts.get("application.haml");
-        FileUtils.writeStringToFile(outputFile, StringUtils.replace(layout, "<%= yield %>", hamlOutput) );
+        
+        FileUtils.writeStringToFile(outputFile, 
+                StringUtils.replace(layout, "<%= yield %>", hamlOutput) );
     }
     
+    /**
+     * Iterates the layouts in the haml/layouts folder and stores the
+     * processed MarkUp into a HashMap
+     */
     private void populateLayouts() {
         JHaml jhaml = new JHaml();
         File file = new File(this.hamlPath + SEPERATOR + "layouts");
         for(File layoutFile: file.listFiles(getFilenameFilter())) {
             try {
-                this.layouts.put(layoutFile.getName(), jhaml.parse(FileUtils.readFileToString(layoutFile)));
+                this.layouts.put(layoutFile.getName(), 
+                        jhaml.parse(FileUtils.readFileToString(layoutFile)));
             }
             catch(IOException ioe) {
-                System.err.println("Unable to convert layout : " + layoutFile.getName());
+                System.err.println("Unable to convert layout : " + 
+                                        layoutFile.getName());
                 ioe.printStackTrace();
             }
         }
         
     }
     
+    /**
+     * Gets the XHTML Output based on the hamlFile given
+     * @param hamlFile the haml file object
+     * @return JSP String
+     * @throws IOException if unable to read the file.
+     */
     private String getHamlOutput(File hamlFile) throws IOException{
         JHaml jhaml = new JHaml();
         return jhaml.parse(FileUtils.readFileToString(hamlFile));
